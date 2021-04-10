01e11638625dced658edad538480ce7b522c3fd2
Rick Drizin <Drizin@users.noreply.github.com>
2021-04-10 19:55:02 -0400

    rename

--- src/InterpolatedLogging.Serilog/InterpolatedLoggingExtensions.cs
+++ src/InterpolatedLogging.Serilog/InterpolatedLoggingExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using InterpolatedLogging;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -9,93 +10,107 @@ namespace Serilog
     /// <summary>
     /// Extend Serilog ILogger with facades using Interpolated strings
     /// </summary>
-    public static class SerilogExtensions
+    public static class InterpolatedLoggingExtensions
     {
-        #region Serilog facades
+        #region Facade Extensions
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public static void InterpolatedVerbose(this ILogger logger, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Verbose))
+            if (!logger.IsEnabled(LogEventLevel.Verbose))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Verbose(msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedVerbose(this ILogger logger, Exception exception, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Verbose))
+            if (!logger.IsEnabled(LogEventLevel.Verbose))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Verbose(exception, msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedDebug(this ILogger logger, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Debug))
+            if (!logger.IsEnabled(LogEventLevel.Debug))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Debug(msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedDebug(this ILogger logger, Exception exception, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Debug))
+            if (!logger.IsEnabled(LogEventLevel.Debug))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Debug(exception, msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedInformation(this ILogger logger, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Information))
+            if (!logger.IsEnabled(LogEventLevel.Information))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Information(msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedInformation(this ILogger logger, Exception exception, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Information))
+            if (!logger.IsEnabled(LogEventLevel.Information))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Information(exception, msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedWarning(this ILogger logger, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Warning))
+            if (!logger.IsEnabled(LogEventLevel.Warning))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Warning(msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedWarning(this ILogger logger, Exception exception, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Warning))
+            if (!logger.IsEnabled(LogEventLevel.Warning))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Warning(exception, msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedError(this ILogger logger, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Error))
+            if (!logger.IsEnabled(LogEventLevel.Error))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Error(msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedError(this ILogger logger, Exception exception, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Error))
+            if (!logger.IsEnabled(LogEventLevel.Error))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Error(exception, msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedFatal(this ILogger logger, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Fatal))
+            if (!logger.IsEnabled(LogEventLevel.Fatal))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Fatal(msg.MessageTemplate, msg.Properties);
         }
         public static void InterpolatedFatal(this ILogger logger, Exception exception, FormattableString message)
         {
-            if (!logger.IsEnabled(Events.LogEventLevel.Fatal))
+            if (!logger.IsEnabled(LogEventLevel.Fatal))
                 return;
             StructuredLogMessage msg = new StructuredLogMessage(message);
             logger.Fatal(exception, msg.MessageTemplate, msg.Properties);
+        }
+        public static void InterpolatedWrite(this ILogger logger, LogEventLevel level, FormattableString message)
+        {
+            if (!logger.IsEnabled(level))
+                return;
+            StructuredLogMessage msg = new StructuredLogMessage(message);
+            logger.Write(level, msg.MessageTemplate, msg.Properties);
+        }
+        public static void InterpolatedWrite(this ILogger logger, LogEventLevel level, Exception exception, FormattableString message)
+        {
+            if (!logger.IsEnabled(level))
+                return;
+            StructuredLogMessage msg = new StructuredLogMessage(message);
+            logger.Write(level, exception, msg.MessageTemplate, msg.Properties);
         }
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         #endregion
